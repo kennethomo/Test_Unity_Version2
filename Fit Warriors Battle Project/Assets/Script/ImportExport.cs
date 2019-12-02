833189561c4dad9ee26da4d76ab5af4597c41812
Kenneth <kennethomo830@gmail.com>
2019-12-01 19:42:29 -0600

    updated game

---
+++ Fit Warriors Battle Project/Assets/Script/ImportExport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+
+public class ImportExport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        using (StreamReader sr = new StreamReader("database.dat"))
+        {
+            string line;
+            while((line = sr.ReadLine())!=null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
